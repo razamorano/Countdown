c01f9ada8a812b4f8c344d01fdd7221642cf95bd
Ramon Zamorano <razamorano@gmail.com>
2020-04-29 19:53:22 +1200

    Initial Commit

---
+++ Countdown/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Countdown
+//
+//  Created by Ramon Zamorano on 29/04/20.
+//  Copyright © 2020 Ramon Zamorano. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Countdown/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Countdown
+//
+//  Created by Ramon Zamorano on 29/04/20.
+//  Copyright © 2020 Ramon Zamorano. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
