27016a1d86e13bacc4ab5c2ab7bd25a17f137cbd
Ramon Zamorano <razamorano@gmail.com>
2020-04-29 20:39:34 +1200

    Initial commit

--- Countdown/ViewController.swift
+++ Countdown/ViewController.swift
@@ -7,20 +7,50 @@
 //
 
 import Cocoa
+import Foundation
 
 class ViewController: NSViewController {
 
+    var OurTimer = Timer()
+    var TimerDisplayed = 0
+    
+    @IBOutlet weak var Label: NSTextField!
+    
+    
+    @IBAction func StartBTN(_ sender: Any) {
+        OurTimer=Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(Action), userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func PauseBTN(_ sender: Any) {
+        OurTimer.invalidate()
+    }
+    
+    
+    
+    @IBAction func ResetBTN(_ sender: Any) {
+        OurTimer.invalidate()
+        TimerDisplayed=0
+        Label.stringValue = "0"
+    }
+    
+    @objc func Action(){
+        TimerDisplayed += 1
+        Label.stringValue = String(TimerDisplayed)
+    }
+    
+    
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
+        //super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
-    override var representedObject: Any? {
-        didSet {
+   //override var representedObject: Any? {
+   //     didSet {
         // Update the view, if already loaded.
-        }
-    }
+    //    }
+   // }
 
 
 }
